bbc1fc9df9d1bc1d13d5a45d4a2c6f30614f79e2
alex4321 <gaussmake@gmail.com>
2023-11-03 23:36:12 +0400

    Fix windows nvdev_docs wrapper

--- nbdev_docs_windows.py
+++ nbdev_docs_windows.py
@@ -1,5 +1,5 @@
-import os
-os.uname = lambda: ("Windows 10", "", "", "", "")
-
-import nbdev.quarto
-nbdev.quarto.nbdev_docs()
+if __name__ == "__main__":
+    import os
+    os.uname = lambda: ("Windows 10", "", "", "", "")
+    import nbdev.quarto
+    nbdev.quarto.nbdev_docs()
